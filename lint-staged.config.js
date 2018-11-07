8d2b5310f66c686744c4f6c2a1399e6a14a21571
Chris Fritz <chrisvfritz@gmail.com>
2018-11-07 11:16:49 -0500

    consolidate config files

--- lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  '*.json': ['prettier --write --loglevel warn', 'git add'],
-  '*.js': ['eslint --fix', 'prettier --write --loglevel warn', 'git add'],
-  '*.vue': ['eslint --fix', 'prettier --write --loglevel warn', 'git add'],
-  '*.md': ['markdownlint', 'prettier --write --loglevel warn', 'git add'],
-  '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged', 'git add'],
-}
